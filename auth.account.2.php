cad374be3df0d6fa368c821966469796738ffae1
ChristinaObrien <50044326+ChristinaObrien@users.noreply.github.com>
2019-04-27 11:06:08 +0000

    Update auth.account.2.php

--- auth.account.2.php
+++ auth.account.2.php
@@ -2,9 +2,7 @@
     // WePay PHP SDK - http://git.io/mY7iQQ
     require 'wepay.php';
     // oauth2 parameters
-    $code = $_GET['code']; // the code parameter from step 2
-    $redirect_uri = "http://www.example.com/oauth2_redirect_uri"; // this is the redirect_uri you used in step 1
-    // application settings
+    $code = $_GET['52sdga231sddd213jj9b']; // the code parameter from step 
     $client_id = 113456789;
     $client_secret = "1a3b5c8d9";
     // change to useProduction for live environments
