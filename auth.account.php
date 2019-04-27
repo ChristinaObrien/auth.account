a0d24bf79841e557ab9c94458986167b01ee5ad8
ChristinaObrien <50044326+ChristinaObrien@users.noreply.github.com>
2019-04-27 09:36:10 +0000

    Create auth.account.php

---
+++ auth.account.php
@@ -0,0 +1,19 @@
+<?php
+    // WePay PHP SDK - http://git.io/mY7iQQ
+    require 'wepay.php';
+    // application settings
+    $client_id = 123456789;
+    $client_secret = "1a3b5c8d9";
+    $access_token = "1a3b5c8d9";
+    // change to useProduction for live environments
+    Wepay::useStaging($client_id, $client_secret);
+    $wepay = new WePay($access_token);
+    // create an account for a user
+    $response = $wepay->request('account/create/', array(
+        'name'          => 'Account Name',
+        'description'   => 'A description for your account.'
+    ));
+    // display the response
+    print_r($response);
+?>
+view raw
